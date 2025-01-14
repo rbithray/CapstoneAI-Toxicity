98fd51361219e4e2dd22ae9efb503416adeeae0c
rbithray <61272208+rbithray@users.noreply.github.com>
2025-01-14 10:42:49 +0100

    typo in loadRTP.py

--- data/loadRTP.py
+++ data/loadRTP.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 
 df = pd.read_json("hf://datasets/allenai/real-toxicity-prompts/prompts.jsonl", lines=True)
-prompts = pd.DataFrame(list(df.prompts))
+prompts = pd.DataFrame(list(df.prompt))
 prompt_text = list(prompts.text)
 with open("prompts.json", "w") as outfile:
     json.dump(prompt_text, outfile, indent=4)
